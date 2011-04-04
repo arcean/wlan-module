95492f8ea2ce77cc93c03b60ce3032662f74797d
Tomasz Pieniazek <t.pieniazek@gazeta.pl>
2011-04-05 01:25:31 +0200

    Yes, our project have a name. #2

--- WlanMaemo.cpp
+++ WlanMaemo.cpp
@@ -1,6 +1,6 @@
 /*
-  Placeholder for the project name...
-  Copyright (C) 2011  Tomasz Pieniążek
+  Wlan-navi project.
+  Copyright (C) 2011  Tomasz Pieniążek & the Wlan-navi team.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
